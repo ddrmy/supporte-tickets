c70b3b4ee74e1ad4b9f23452cebc9495ca053180
Gabriel Sarti Fernandes Roberti <gabriel.roberti@onplug.com.br>
2026-06-05 10:29:06 -0300

    add: script with regex to replace and find everyone url

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -5,10 +5,12 @@ const database = new Database()
 
 export function routeHandler(req, res){
    const route = routes.find((route) => {
-    return route.method === req.method && route.path === req.url
+    return route.method === req.method && route.path.test(req.url)
    })
 
    if(route){
+      const routeParams = request.url.match(route.path)
+      console.log(routeParams)
     return route.controller({ req, res, database, database })
    }
 
