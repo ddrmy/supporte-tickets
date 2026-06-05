999f77c9cbde00c54fb9a8841da60e0b8c3a94af
Gabriel Sarti Fernandes Roberti <gabriel.roberti@onplug.com.br>
2026-06-05 11:22:23 -0300

    add: script to extract query params and req only status

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -2,6 +2,8 @@
 
 export function index({req, res, database}) {
 
+    const { status } = req.query
+    console.log(status)
     const tickets = database.select("tickets")
 
     return res.end(JSON.stringify(tickets))
