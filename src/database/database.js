c5d4cbc82f4121a8ab729a14ec94a4c8e9dd708c
Gabriel Sarti Fernandes Roberti <gabriel.roberti@onplug.com.br>
2026-06-05 13:53:28 -0300

    add: scritp to remove ticket

--- src/database/database.js
+++ src/database/database.js
@@ -1,58 +1,67 @@
-import fs from "node:fs/promises";
+import fs from "node:fs/promises"
 
-const DATABASE_PATH = new URL("db.json", import.meta.url);
+const DATABASE_PATH = new URL("db.json", import.meta.url)
 
 export class Database {
-  #database = {};
+  #database = {}
 
   constructor() {
     fs.readFile(DATABASE_PATH, "utf-8")
       .then((data) => {
-        this.#database = JSON.parse(data);
+        this.#database = JSON.parse(data)
       })
       .catch(() => {
-        this.#persist();
-      });
+        this.#persist()
+      })
   }
 
   #persist() {
-    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database));
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database))
   }
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
-      this.#database[table].push(data);
+      this.#database[table].push(data)
     } else {
-      this.#database[table] = [data];
+      this.#database[table] = [data]
     }
 
-    this.#persist();
+    this.#persist()
   }
 
   select(table, filters) {
-    let data = this.#database[table] ?? [];
+    let data = this.#database[table] ?? []
 
     if (filters) {
       data = data.filter((row) => {
         return Object.entries(filters).some(([key, value]) => {
-          return row[key].toLowerCase().includes(value.toLowerCase());
-        });
-      });
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
     }
-    return data;
+    return data
   }
 
   update(table, id, data) {
-    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
 
-    if(rowIndex > -1){
-        this.#database[table][rowIndex] = {
-            ...this.#database[table][rowIndex],
-            ...data
-        }
-        this.#persist()
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = {
+        ...this.#database[table][rowIndex],
+        ...data,
+      }
+      this.#persist()
     }
 
     console.log(rowIndex)
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
   }
 }
